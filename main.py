3936d0c3db1adb36c556d956656a817ef8fa5b5b
Weisman <weisman123@outlook.com>
2022-10-08 00:59:43 -0700

    changed channel to env var

--- main.py
+++ main.py
@@ -9,14 +9,14 @@ bot = commands.Bot(command_prefix='$', intents=discord.Intents().all())
 async def on_ready():
     print('Logged in as {0.user}'.format(bot))
     await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="Weisman's Bots"))
-    logChannel = bot.get_channel(906780579872272424)
+    logChannel = bot.get_channel(os.getenv('CHANNEL'))
     await logChannel.send("The Watcher is online.")
 
 @bot.event
 async def on_member_update(before, after):
     if before.bot:
         if before.status.name != after.status.name:
-            logChannel = bot.get_channel(906780579872272424)
+            logChannel = bot.get_channel(os.getenv('CHANNEL'))
             await logChannel.send(f"{before.name} is {after.status.name}.")
 
 keep_alive()
